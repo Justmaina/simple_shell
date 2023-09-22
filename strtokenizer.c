23272f2f4eb383e18050b33a4b2d032532318405
Justmaina <122853510+Justmaina@users.noreply.github.com>
2023-09-22 11:14:20 +0300

    Update strtokenizer.c

--- strtokenizer.c
+++ strtokenizer.c
@@ -1,41 +1,42 @@
 #include "header.h"
 /**
- * tokenize - this function separate the string using a designed delimiter
- * @data: a pointer to the program's data
- * Return: an array of the different parts of the string
- */
-void tokenize(program_info *data)
+ * _strtok - separates strings with delimiters
+ * @line: It´s pointer to array we receive in getline.
+ * @delim: It´s characters we mark off string in parts.
+ * Return: A pointer to the created token
+*/
+char *_strtok(char *line, char *delim)
 {
-	char *delimiter = " \t";
-	int i, j, tracker = 2, length;
+	int j;
+	static char *str;
+	char *cpstr;
 
-	length = str_length(data->input_line);
-	if (length)
+	if (line != NULL)
+		str = line;
+	for (; *str != '\0'; str++)
 	{
-		if (data->input_line[length - 1] == '\n')
-			data->input_line[length - 1] = '\0';
-	}
-
-	for (i = 0; data->input_line[i]; i++)
-	{
-		for (j = 0; delimiter[j]; j++)
+		for (j = 0; delim[j] != '\0'; j++)
 		{
-			if (data->input_line[i] == delimiter[j])
-				tracker++;
+			if (*str == delim[j])
+			break;
+		}
+		if (delim[j] == '\0')
+			break;
+	}
+	cpstr = str;
+	if (*cpstr == '\0')
+		return (NULL);
+	for (; *str != '\0'; str++)
+	{
+		for (j = 0; delim[j] != '\0'; j++)
+		{
+			if (*str == delim[j])
+			{
+				*str = '\0';
+				str++;
+				return (cpstr);
+			}
 		}
 	}
-
-	data->tokens = malloc(tracker * sizeof(char *));
-	if (data->tokens == NULL)
-	{
-		perror(data->program_name);
-		exit(errno);
-	}
-	i = 0;
-	data->tokens[i] = str_duplicate(_strtok(data->input_line, delimiter));
-	data->command_name = str_duplicate(data->tokens[0]);
-	while (data->tokens[i++])
-	{
-		data->tokens[i] = str_duplicate(_strtok(NULL, delimiter));
-	}
+	return (cpstr);
 }
