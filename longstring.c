f1a0c7da2cd69589e837e76d6ae89a25ee9df756
Justmaina <122853510+Justmaina@users.noreply.github.com>
2023-09-22 10:25:18 +0300

    Create longstring.c

---
+++ longstring.c
@@ -0,0 +1,36 @@
+#include "header.h"
+
+/**
+ * long_to_string - convert number to a string.
+ * @number: number to concert
+ * @string: buffer to save the num as string.
+ * @base: base for concersion
+ *
+ * Return: Nothing.
+ */
+void long_to_string(long number, char *string, int base)
+{
+	int index = 0, inNegative = 0;
+	long cociente = number;
+	char letters[] = {"0123456789abcdef"};
+
+	if (cociente == 0)
+		string[index++] = '0';
+
+	if (string[0] == '-')
+		inNegative = 1;
+
+	while (cociente)
+	{
+		if (cociente < 0)
+			string[index++] = letters[-(cociente % base)];
+		else
+			string[index++] = letters[cociente % base];
+		cociente /= base;
+	}
+	if (inNegative)
+		string[index++] = '-';
+
+	string[index] = '\0';
+	str_reverse(string);
+}
